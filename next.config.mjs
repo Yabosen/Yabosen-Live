81f2e7711732f02b07bc83bb6c8e7a45287bfa1f
Aquamarine Hoshino <cozoral@outlook.com>
2025-10-19 03:57:10 +0200

    asdd

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,15 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/discord',        // the path on your site
+        destination: 'https://discord.gg/DgavuHSaWA',  // your Discord link
+        permanent: true,          // false = temporary redirect (307), true = permanent (308)
+      },
+         ];
+  },
 }
 
 export default nextConfig
