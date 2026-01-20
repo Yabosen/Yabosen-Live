18818bba8d694f5d79ca4f3dbaa3d5fb18c38dcb
Otto_606 <147689545+OttoApocalypse69@users.noreply.github.com>
2026-01-20 16:30:19 +0200

    blyat 6.2.0.000of2

--- components/faceit-widget.tsx
+++ components/faceit-widget.tsx
@@ -2,10 +2,11 @@ export function FaceitWidget() {
     return (
         <div className="mt-2">
             <iframe
-                src="https://faceitlivestats.win/widget-compact-today?hideRank=false&hideChallenger=false&hideWinsLosses=true&rounded=false&transparent=false&isGiant=false&nickname=Yabosen"
-                style={{ width: '250px', height: '110px', border: 'none' }}
+                src="https://faceitlivestats.win/widget-compact-today?hideRank=false&hideChallenger=false&hideWinsLosses=true&rounded=false&transparent=true&isGiant=false&nickname=Yabosen"
+                style={{ width: '100%', maxWidth: '250px', height: '110px', border: 'none', overflow: 'hidden' }}
                 title="Faceit Stats"
                 loading="lazy"
+                scrolling="no"
             />
         </div>
     )
